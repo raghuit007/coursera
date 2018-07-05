7175fe84bb82147428a47373cf345560275fed91
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2018-07-05 17:01:55 +0100

    Update rabin-karp.java
    Version 2 of the code. Please test it

--- rabin-karp.java
+++ rabin-karp.java
@@ -142,3 +142,132 @@ public class HashSubstring {
 }
 
 
+/**
+ import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class HashSubstring {
+
+    private static FastScanner in;
+    private static PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        in = new FastScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        printOccurrences(getOccurrences(readInput()));
+        out.close();
+    }
+
+    private static Data readInput() throws IOException {
+        String pattern = in.next();
+        String text = in.next();
+        return new Data(pattern, text);
+    }
+
+    private static void printOccurrences(List<Integer> ans) throws IOException {
+        for (Integer cur : ans) {
+            out.print(cur);
+            out.print(" ");
+        }
+    }
+
+    private static List<Integer> getOccurrences(Data input) {
+        String s = input.pattern, t = input.text;
+        //calcluate the hash before hand
+        
+        int h = s.hashCode();
+        int m = s.length(), n = t.length();
+        
+        
+        List<Integer> occurrences = new ArrayList<Integer>();
+        double [] hashes = new double[n-m+1];
+        
+        // change the code here to implement fast version of Rabin-Karp algorithm
+        // first lets try to get it working with the default hash computed with Java (hash code)
+        
+        // we need to pre compute the hash, rather than calculating it on the fly
+        //else there is a time limit exceeded error
+        //get a prime number >> m*n (p)
+        
+        double p =492876847D;
+        double rand = 256D;
+        
+        double pHash = polyHash(s,p,rand);
+        hashes =  preComputeHash(t,m,p,rand);
+        
+        //for (int j=0;j<)
+        
+        
+        return occurrences;
+        }
+
+	private static double[] preComputeHash(String t, int m, double p,double rand) {
+		
+		double preHashes [] = new double[t.length()-m+1];
+		String firstSub = t.substring(0,m);
+		
+		preHashes[preHashes.length-1] = polyHash(firstSub,p,rand);
+		
+		for (int i=t.length()-m-1;i>=0;i++) {
+			//(d*(t - txt.charAt(i)*h) + txt.charAt(i+M))%q;
+			preHashes[i] = ((rand*(preHashes[i+1]-t.charAt(i)+ t.charAt(i+m))))%p;
+		}
+		return preHashes;
+		
+	}
+        
+        
+        
+     private static double polyHash(String s, double p,double rand) {
+		// TODO Auto-generated method stub
+    	double hash =0D;
+    	
+    	for (int i=1;i<=s.length()-1;i++) {
+    		hash = (rand*hash + s.charAt(i))%p;
+    	}
+    	
+    	
+    	
+		return hash;
+	}
+
+
+
+	static class Data {
+        String pattern;
+        String text;
+        public Data(String pattern, String text) {
+            this.pattern = pattern;
+            this.text = text;
+        }
+    }
+
+    static class FastScanner {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public FastScanner() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+}
+
+
+**/
+
+
